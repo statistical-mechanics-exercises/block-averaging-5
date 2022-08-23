d5b7c87255c9571bd3e03089dbffb06c64bc4a71
Gareth Tribello <gareth.tribello@gmail.com>
2022-08-23 16:00:21 +0100

    Using Autofeedback for tests

--- README.md
+++ README.md
@@ -12,8 +12,16 @@ def block_average( M, data ) :
     return error
 ```
 
-You will see that I have also called this function multiple times with different block sizes and made a list containing the size of the error for each of these block sizes.  I have then plotted a graph showing the size of the error against the block size.
+You should then use the function you have written to plot a graph that shows how the size of the error depends on the length of the blocks.  In drawing this graph you should calculate the error when block averages with the following lengths 
+are used in the calculation of the error:
 
-Use what you have learnt in the exercise to complete the function and look at the graph produced.  
+```python
+xvals = [10,20,30,40,60,100,120,200,300,400]
+```
 
-You should see that error is initially small.  It will then grow as the size of the various blocks is increased before plateauing to a constant value. 
+The x coordinates of the points of in your graph should be equal to the numbers in the list above.  The y-coordinates of these points should then be the corresponding values of the error for that size of block.  The y value for the point at x=10 is thus
+the error that is calculated from block averages that are calculated from the first 10, second 10 points and so on.  In practise you can calculate these y-values by using the function `block_average` that you will have written.
+
+You should see that error is initially small.  It will then grow as the size of the various blocks is increased before plateauing to a constant value.
+
+The x axis label of your graph should be "Size of blocks" and the y axis label should be "Error" 
--- testing/test_main.py
+++ testing/test_main.py
@@ -7,21 +7,34 @@ except:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
     from AutoFeedback.funcchecks import check_func
 
+import AutoFeedback.plotchecks as pc
+from AutoFeedback.plotclass import line
+
 myeng = np.loadtxt("energies")[:,1]
+xvals, yvals, k = [10,20,30,40,60,100,120,200,300,400], np.zeros(10), 0
+for bb in xvals :
+    nblocks = int( len( eng ) / bb )
+    myaverage, mysq = 0, 0
+    for i in range(nblocks) :
+        myblocks = sum( myeng[i*bb:(i+1)*bb] ) / bb
+        myaverage = myaverage + myblocks
+        mysq = mysq + myblocks*myblocks
+
+    mysq, myaverage = mysq / nblocks, myaverage / nblocks
+    myvar = ( nblocks / (nblocks - 1) )*( mysq - myaverage*myaverage )
+    yvals[k] = np.sqrt( myvar / nblocks )
+    k = k + 1
+
+line1 = line(xvals,yvals)
+axislabels=["Size of blocks", "Error"]
 
 class UnitTests(unittest.TestCase) :
     def test_blockVals(self) :
         inputs, outputs = [], []
-        for bb in [10,20,30,40,60,100,120,200,300,400] : 
-            inputs.append((bb,myeng,))
-            nblocks = int( len( eng ) / bb ) 
-            myaverage, mysq = 0, 0
-            for i in range(nblocks) :
-                myblocks = sum( myeng[i*bb:(i+1)*bb] ) / bb 
-                myaverage = myaverage + myblocks 
-                mysq = mysq + myblocks*myblocks
-  
-            mysq, myaverage = mysq / nblocks, myaverage / nblocks
-            myvar = ( nblocks / (nblocks - 1) )*( mysq - myaverage*myaverage )
-            outputs.append( np.sqrt( myvar / nblocks ) )
+        for i in len(xvals) : 
+            inputs.append((xvals[i],myeng,))
+            outputs.append( yvals[i] )
         assert( check_func('block_average',inputs, variables ) )
+
+   def test_plot(self):
+       assert( pc.check_plot([line1],explabels=axislabels,explegend=False,output=True) )
